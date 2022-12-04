baa93339e82101786cf2fd71eba4ef969db696e7
Vinura de Silva <m.v.r.desilva@gmail.com>
2022-12-04 20:55:54 +0530

    ADD SUPPLIER

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,13 +2,61 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+class LoginActivity extends AppCompatActivity {
 
-    @Override
+    private TextView textView;
+    private EditText editemail, editpassword;
+    private Button login;
+    DBHelper DB;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editemail = findViewById(R.id.EmailAddress);
+        editpassword = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        DB = new DBHelper(this);
+
+        login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this,"Login Successful...", Toast.LENGTH_SHORT).show();
+                String email = editemail.getText().toString();
+                String pass = editpassword.getText().toString();
+
+                if(TextUtils.isEmpty(email)|| TextUtils.isEmpty(pass))
+                    Toast.makeText(LoginActivity.this,"Fields cannot be empty", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkuserpass = DB.checkpassword(email,pass);
+                    if(checkuserpass == true){
+                        Toast.makeText(LoginActivity.this,"Login Successful...", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivity(intent);
+                    }
+                    else{
+                        Toast.makeText(LoginActivity.this,"Login failed... Try again....",Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -2,13 +2,83 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText username,password,email, repass;
+    Button reg;
+    TextView sin;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.userName);
+        password = findViewById(R.id.password);
+        email = findViewById(R.id.EmailAddress);
+        reg = (Button) findViewById(R.id.regBtn);
+        sin = findViewById(R.id.loginText);
+        DB = new DBHelper(this);
+        repass = findViewById(R.id.repassword);
+
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(RegisterActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                String user = username.getText().toString();
+                String uemail = email.getText().toString();
+                String pass = password.getText().toString();
+
+                if(TextUtils.isEmpty(user)|| TextUtils.isEmpty(pass) || TextUtils.isEmpty(uemail))
+                    Toast.makeText(RegisterActivity.this, "Cannot be Empty", Toast.LENGTH_SHORT).show();
+
+                else{
+                    if(pass.equals(repass)){
+                        Boolean checkuser = DB.checkuseremail(user);
+                        if(checkuser == false){
+                            Boolean insert = DB.insertData(user,pass, uemail);
+                            if(insert == true){
+                                Toast.makeText(RegisterActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+                                startActivity(intent);
+                            }
+                            else{
+                                Toast.makeText(RegisterActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "User already Exists ", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }else{
+                        Toast.makeText(RegisterActivity.this, "Password are not matching", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+        sin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
     }
 }
